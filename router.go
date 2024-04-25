e9fac20ca72ba3d7b77089c9772f31efa75faa52
evihslimited <evihslimited6@gmail.com>
2024-04-25 00:59:08 -0700

    setup the route and router

--- router.go
+++ router.go
@@ -1,6 +1,37 @@
 package jango
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+type Route struct {
+	Path    string
+	Handler http.HandlerFunc
+	method  string
+	name    string
+}
+
+func Path(path string, handler http.HandlerFunc, method string) *Route {
+	return &Route{
+		Path:    path,
+		Handler: handler,
+		method:  method,
+	}
+}
+
+func (rte *Route) Name(name string) *Route {
+	rte.name = name
+	return rte
+}
+
+func (rte *Route) Handle(rter *Router) {
+	rter.RoutePaths[rte.name] = rte.Path
+	if rte.method != "" {
+		rte.Path = fmt.Sprintf("%v %v", rte.method, rte.Path)
+	}
+	rter.Mux.HandleFunc(rte.Path, rte.Handler)
+}
 
 type Router struct {
 	Mux         *http.ServeMux
@@ -8,4 +39,33 @@ type Router struct {
 	version     string
 	middlewares []func(http.Handler) http.Handler
 	RoutePaths  map[string]string
+}
+
+func (rter *Router) VersionString(version_string string) *Router {
+	rter.version = version_string
+	return rter
+}
+
+func (rter *Router) Version(version int) *Router {
+	switch rter.version {
+	case "":
+		rter.version = fmt.Sprintf("/api/%v", version)
+	default:
+		rter.version = fmt.Sprintf("%v/%v", rter.version, version)
+	}
+	return rter
+}
+
+func (rter *Router) Prefix(prefix string) *Router {
+	rter.prefix = rter.version + prefix
+	return rter
+}
+
+func (rter *Router) SubPrefix(sub_prefix string) *Router {
+	rter.prefix = rter.prefix + sub_prefix
+	return rter
+}
+
+func (rter *Router) Middlewares(middlewares ...func(http.Handler) http.Handler) {
+	rter.middlewares = append(rter.middlewares, middlewares...)
 }
